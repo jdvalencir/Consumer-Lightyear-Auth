f4e0da7b324c907f0e47d23b92664dffe2d10422
Julián David Valencia <julianv302@gmail.com>
2025-05-07 01:17:02 -0500

    fix: change env pass name

--- config/db/db.config.js
+++ config/db/db.config.js
@@ -4,14 +4,12 @@ if (process.env.NODE_ENV === 'development') {
     process.loadEnvFile('.env');
   }
 
-console.log('DB_HOST:', process.env.DB_HOST, 'DB_NAME:', process.env.DB_NAME, 'DB_USER:', process.env, 'DB_USER', 'DB_PASSWORD:', process.env);
-
 // sequelize.js
 export const sequelize = new Sequelize({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   dialect: 'postgres',
   logging: false,
 });

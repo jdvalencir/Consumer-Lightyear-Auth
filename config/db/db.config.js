2e3f7396c63550e179e79dd743816d8e04a1f213
Julián David Valencia <julianv302@gmail.com>
2025-05-07 01:11:11 -0500

    log: add logging to validate password

--- config/db/db.config.js
+++ config/db/db.config.js
@@ -3,7 +3,9 @@ import { Sequelize } from 'sequelize';
 if (process.env.NODE_ENV === 'development') {
     process.loadEnvFile('.env');
   }
-  
+
+console.log('DB_HOST:', process.env.DB_HOST, 'DB_NAME:', process.env.DB_NAME, 'DB_USER:', process.env, 'DB_USER', 'DB_PASSWORD:', process.env);
+
 // sequelize.js
 export const sequelize = new Sequelize({
   host: process.env.DB_HOST,
